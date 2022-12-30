dc3afcd1445dde080560fd45b158d5f1423113f2
Toomore Chiang <toomore0929@gmail.com>
2022-12-30 04:01:22 +0000

    Change to demo serve

--- script/serve.sh
+++ script/serve.sh
@@ -12,4 +12,5 @@ if [ "_${PAGES_REPO_NWO}_" == "__" ]; then
 export PAGES_REPO_NWO=publiccodenet/standard
 fi
 
-bundle exec jekyll serve --livereload
+#bundle exec jekyll serve --livereload --host 127.0.0.1 --port 6015
+bundle exec jekyll serve --host https://lyzez5.toomore.net --port 6015
